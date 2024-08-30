c9cd54dc1c3de77cfce2e47c65122513dcbda4da
vpslombok <vpslombokntb@gmail.com>
2024-08-30 17:47:06 +0800

    upload

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+// Koneksi database php
+$host = 'bdeaoblcrmhzuzixz9lu-mysql.services.clever-cloud.com'; // Ganti dengan IP server MySQL
+$user = 'uaihieeeuy2yjzsi'; // Ganti dengan username MySQL Anda
+$password = 'aVw6FpQ2JcmOlbvLGWgt'; // Ganti dengan password MySQL Anda
+$dbname = 'bdeaoblcrmhzuzixz9lu';
+
+// Membuat koneksi
+$conn = new mysqli($host, $user, $password, $dbname);
+
+// Memeriksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
